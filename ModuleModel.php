485e5b400c4f3cf8f4073f417fe5243667f3c0f5
AMWOLLO VICTOR <amwollo@yahoo.com>
2021-01-16 20:25:10 +0300

    Add the Model file

---
+++ ModuleModel.php
@@ -0,0 +1,54 @@
+<?php
+require_once "ModuleBuilderInterface.php";
+class ModuleModel implements ModuleBuilderInterface
+{
+
+    private $name;
+    private $side;
+    private $extension;
+    private $content;
+    private $version;
+    private $path;
+
+    public function name($name) {
+        $this->name = $name;
+        return $this;
+
+    }
+    public function side($side) {
+        $this->side = $side;
+        return $this;
+    }
+    public function extension($extension) {
+        $this->extension = $extension;
+        return $this;
+    }
+
+    public function path($path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    public function content($content) {
+        $this->content = $content;
+        return $this;
+    }
+    public function version($version) {
+        $this->version = $version;
+        return $this;
+    }
+
+    public function generateFile() {
+        $output_folder = 'output/' . $this->name.'/upload/'. $this-> side . '/' . $this->path;
+
+        if (!file_exists($output_folder)) {
+            mkdir($output_folder, 0777, true);
+        }
+
+        $filename = $output_folder.$this->name.'.'. $this->extension;
+
+        file_put_contents($filename, $this->content);     // Save our content to the file.
+    }
+    
+}
